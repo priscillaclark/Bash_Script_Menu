8ffd1ee38345188d71b2046529a7d77ba1f8cbf0
priscilla clark <priscillaclark5@gmail.com>
2023-09-27 22:46:08 -0300

    add submenu inside option 1

--- contacts.sh
+++ contacts.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+function_create()
+{
+read -r -p "
+Enter name: " NAME
+#-p is anti /
+read -r -p "Enter last name: " LASTNAME
+
+read -r -p "Enter phone: " PHONE
+
+read -r -p "Enter mail: " MAIL
+
+NEWCONTACT = $NAME + $LASTNAME + $PHONE + $MAIL
+read $CONTACT | grep contact.txt
+}
+
 echo "
 Menu:
 1) Create
@@ -11,30 +26,24 @@ read -n 1 -r -p "Enter an option: " OPTION
 case $OPTION in
 
  1)
-	# function_create()
-exit 0	
+	 function_create
 ;;
 
  2)
 	# function_search()
-exit 0
 	;;
 3)
 	echo "
 On the main menu type [1] for creating a new person
 type [2] for searching a new person
 any other input is invalid"
-exit 0
 
 	;;
 
   *)
 	echo "
 Error: incorrect option, please retry"
-
 ;;
+
 esac
 
-#function_create(){
-
-#}
