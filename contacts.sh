9d342f85a2be62e326110d2769cdc493c0d6cffa
priscilla clark <priscillaclark5@gmail.com>
2023-11-14 01:22:10 -0300

    typo fixes and improvements in search function mainly + keyword search added

--- contacts.sh
+++ contacts.sh
@@ -4,7 +4,6 @@ function_create() {
 	read -r -p "
 Enter name: " name
 	# -p is anti /
-
 	# -n checks the string variable is not empty?
 	# -z checks the string variable is empty
 
@@ -22,27 +21,29 @@ Enter name: " name
 
 function_search() {
 
-	contacts_file="contacts.txt"
+	contacts_file="contact.txt"
+	found=""
 
 	read -r -p "
-Enter contact name to search for: " givencontact
+Enter contact name or keyword to search for: " givencontact
 
-	#fix this so that user can sear for kewwords not only name
-
-	# check string is empty
+		# checks if length of string is zero
 	if [ -z "$givencontact" ]; then
 		echo "
-Please give a contact name"
-	#check contact file is not empty
-	elif [ ! -s contact.txt ]; then
-		# -s checks the size of a file is greater than 0, means it's not empty
-		echo "
-The contact file is empty, please create a contact before searching"
+Please provide a contact name or a keyword"
 
-		#why cant i put the regular expression directly on the elif who knows
-		found=$(grep -i "$givencontact" "$contacts_file")
-		#-i to ignore case sensitivity
-	elif [ -n "$found" ]; then
+		# -e check if file exists
+	elif [ ! -e "contacts_file" ]; then
+		echo "
+The contact file does not exist, please create a contact before searching"
+
+	else
+		# -i to ignore case sensitivity
+		# -E to enable extended regular expressions
+
+		found=$(grep -iE "$givencontact" "$contacts_file")
+
+	if [ -n "$found" ]; then
 		#-n check not empty
 
 		echo "
@@ -50,9 +51,11 @@ Contact found:"
 		echo "$found"
 	else
 		echo "
-Contact not found"
+No contact was found for '$givencontact'"
 
 	fi
+	
+fi
 }
 
 while true; do
@@ -78,9 +81,9 @@ while true; do
 		;;
 	3)
 		echo "
-	On the main menu type [1] for creating a new contact
-	type [2] for searching a new contact
-	any other input is invalid"
+		On the main menu type [1] for creating a new contact
+		type [2] for searching a new contact
+		any other input is invalid"
 
 		;;
 	4)
@@ -90,7 +93,7 @@ while true; do
 
 	*)
 		echo "
-	Error: incorrect option, please retry"
+		Error: incorrect option, please retry"
 		;;
 	esac
 
